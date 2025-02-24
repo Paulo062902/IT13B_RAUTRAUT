0de97c2a988d9c9d5859722fc8d2b74b57fb386f
Jologscodes <dhemarodriguez53@gmai.com>
2025-02-24 10:52:08 +0800

    update

---
+++ JavaApplication8/src/fraktes.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+public class fraktes {
+ public static void main(String [] args){
+//     Scanner scanner = new scanner( System.in);
+// Nested Loops 
+    for (int x = 1; x<=10; x++){
+        for(int y=1; y <=10; y++){
+          System.out.printf("%4d" , x*y);{
+       
+        }
+        System.out.println();
+    }
+        
+     
+     
+     
+ }
+}
+}
